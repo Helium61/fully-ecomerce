ce6f0556da36052963d8ea026f5f928c067c77b1
Helium61 <heliumera4.0@gmail.com>
2024-06-10 18:16:45 +0500

    second

--- src/data/headphoneList.jsx
+++ src/data/headphoneList.jsx
@@ -5,56 +5,56 @@ const headphoneList = [
       id: 1,
       name: 'Wireless Headphones',
       description: 'Enjoy crystal-clear sound with our wireless headphones.',
-      image: 'public/photo-1546435770-a3e426bf472b.avif', // Update with actual image paths
+      image: 'Images/photo-1546435770-a3e426bf472b.avif', // Update with actual image paths
       price: 199.99,
     },
     {
       id: 2,
       name: 'Noise Cancelling Headphones',
       description: 'Experience tranquility with noise-cancelling technology.',
-      image: 'public/photo-1496957961599-e35b69ef5d7c.avif', // Update with actual image paths
+      image: 'Images/photo-1496957961599-e35b69ef5d7c.avif', // Update with actual image paths
       price: 299.99,
     },
     {
       id: 3,
       name: 'Bluetooth Headphones',
       description: 'Seamlessly connect with Bluetooth headphones.',
-      image: 'public/photo-1505751171710-1f6d0ace5a85.avif', // Update with actual image paths
+      image: 'Images/photo-1505751171710-1f6d0ace5a85.avif', // Update with actual image paths
       price: 149.99,
     },
     {
       id: 4,
       name: 'Over-Ear Headphones',
       description: 'Comfort and quality for long listening sessions.',
-      image: 'public/photo-1599669454699-248893623440.avif', // Update with actual image paths
+      image: 'Images/photo-1599669454699-248893623440.avif', // Update with actual image paths
       price: 249.99,
     },
     {
       id: 5,
       name: 'In-Ear Headphones',
       description: 'Compact design with powerful sound.',
-      image: 'public/photo-1545127398-14699f92334b.avif', // Update with actual image paths
+      image: 'Images/photo-1545127398-14699f92334b.avif', // Update with actual image paths
       price: 99.99,
     },
     {
         id: 6,
         name: 'In-Ear Headphones',
         description: 'Compact design with powerful sound.',
-        image: 'public/photo-1505740420928-5e560c06d30e.avif', // Update with actual image paths
+        image: 'Images/photo-1505740420928-5e560c06d30e.avif', // Update with actual image paths
         price: 99.99,
       },
       {
         id:7,
         name: 'In-Ear Headphones',
         description: 'Compact design with powerful sound.',
-        image: 'public/premium_photo-1680346528789-0ffcc13f5ebf.avif', // Update with actual image paths
+        image: 'Images/premium_photo-1680346528789-0ffcc13f5ebf.avif', // Update with actual image paths
         price: 99.99,
       },
       {
         id: 8,
         name: 'In-Ear Headphones',
         description: 'Compact design with powerful sound.',
-        image: 'public/photo-1496957961599-e35b69ef5d7c.avif', // Update with actual image paths
+        image: 'Images/photo-1496957961599-e35b69ef5d7c.avif', // Update with actual image paths
         price: 99.99,
       },
   ];
